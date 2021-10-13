515d6cc4681b7a4a9eab03142d04be4477d49a26
green60ninja <82245459+green60ninja@users.noreply.github.com>
2021-10-12 21:13:40 -0500

    Added exception handling and reformatted code - 10/12/21

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 from tkinter import *
+from tkinter import messagebox
 from PIL import Image, ImageTk
 from pynput import keyboard
 import threading
@@ -19,7 +20,7 @@ G = 6.67430 * pow(10, -11)  # Newtonian constant of gravitation
 t = 0  # TIME (increments every second)
 
 # Declaring the objects for the moon and the satellite in orbit.
-# TODO: Use Keplerian element conversion to find position and velocity for satellite
+# TODO: Use Keplerian element conversion to find latitude and longitude for satellite
 moon = OrbitObj(mass=7.34767309 * pow(10, 22))
 sat = OrbitObj(mass=1916)  # Mass: (National Aeronautics and Space Administration, 2009)
 
@@ -27,6 +28,12 @@ mu = G * moon.mass
 thrust = 44037.39       # Thrust variable
 amount_change = 10      # How much thrust changes for each button press
 window_width = 1500     # For rendering the canvas & map.
+
+a = 0
+e = 0
+inclin = 0
+omega = 0
+w = 0
 
 
 # ---------------THRUST CODE---------------
@@ -76,27 +83,22 @@ t_string = StringVar(root, '0')
 label = Label(root, text="Keplerian Elements:")
 label.pack()
 
-a = 0
 semimajor_entry = Entry(root, width=25)
 semimajor_entry.insert(0, "Semi-major axis")
 semimajor_entry.pack()
 
-e = 0
 eccentricity_entry = Entry(root, width=25)
 eccentricity_entry.insert(0, "Eccentricity")
 eccentricity_entry.pack()
 
-inclin = 0
 inclination_entry = Entry(root, width=25)
 inclination_entry.insert(0, "Inclination")
 inclination_entry.pack()
 
-omega = 0
 ascension_entry = Entry(root, width=25)
 ascension_entry.insert(0, "Right ascension of ascending node")
 ascension_entry.pack()
 
-w = 0
 perigee_entry = Entry(root, width=25)
 perigee_entry.insert(0, "Argument of perigee")
 perigee_entry.pack()
@@ -109,18 +111,24 @@ def submit():
     # print(inclination_entry.get())
     # print(perigee_entry.get())
 
-    a = semimajor_entry.get()
-    e = eccentricity_entry.get()
-    inclin = inclination_entry.get()
-    omega = ascension_entry.get()
-    w = perigee_entry.get()
+    try:
+        a = float(semimajor_entry.get())
+        e = float(eccentricity_entry.get())
+        inclin = float(inclination_entry.get())
+        omega = float(ascension_entry.get())
+        w = float(perigee_entry.get())
+    except ValueError:
+        # TODO: Make better exception handling for invalid input.
+        # Currently, the program terminates if an integer is not entered.
+        messagebox.showerror(title="ERROR", message="INVALID INPUT FOR ENTRY")
+        root.destroy()
 
     frame = Frame(root, height=20, width=100).pack()
-    l1 = Label(frame, text="Semi-major axis: "+a).pack()
-    l2 = Label(frame, text="Eccentricity: "+e).pack()
-    l3 = Label(frame, text="Inclination: "+inclin).pack()
-    l3 = Label(frame, text="Right ascension of ascending node: " + omega).pack()
-    l4 = Label(frame, text="Argument of perigee: "+w).pack()
+    l1 = Label(frame, text="Semi-major axis: "+str(a)).pack()
+    l2 = Label(frame, text="Eccentricity: "+str(e)).pack()
+    l3 = Label(frame, text="Inclination: "+str(inclin)).pack()
+    l3 = Label(frame, text="Right ascension of ascending node: " + str(omega)).pack()
+    l4 = Label(frame, text="Argument of perigee: "+str(w)).pack()
 
     t1.start()
     l5 = Label(frame, textvariable=t_string).pack()
