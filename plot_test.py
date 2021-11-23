e2dfa4926d8e41869270f760036f2bd4c0a9a852
green60ninja <82245459+green60ninja@users.noreply.github.com>
2021-11-23 16:30:11 -0600

    Using 50,000th LRO orbital elements - 11/23/21

--- plot_test.py
+++ plot_test.py
@@ -1,6 +1,7 @@
 from astropy.units.quantity import Quantity
 from poliastro.bodies import Moon
 from poliastro.constants.general import J2000
+from poliastro.frames.enums import Planes
 from poliastro.plotting.static import StaticOrbitPlotter
 from poliastro.twobody.orbit import Orbit
 import matplotlib.pyplot as plt
@@ -8,15 +9,19 @@ import matplotlib.pyplot as plt
 def plot_lro_orbit():
     lro = Orbit.from_classical(
         attractor=Moon,
-        a=Quantity(value=2011.343, unit='km'),
-        ecc=Quantity(value=0.635485),
-        inc=Quantity(value=90.016, unit='deg'),
-        raan=Quantity(value=88.74, unit='deg'),
-        argp=Quantity(value=336.655, unit='deg'),
-        nu=Quantity(value=0, unit='deg')
+        a=Quantity(value=1795.224, unit='km'),
+        ecc=Quantity(value=0.019296),
+        inc=Quantity(value=85.780, unit='deg'),
+        raan=Quantity(value=128.208, unit='deg'),
+        argp=Quantity(value=246.531, unit='deg'),
+        nu=Quantity(value=0, unit='deg'),
+        plane=Planes.BODY_FIXED,
+        epoch=J2000
     )
-    
-    frame = StaticOrbitPlotter()
-    # frame.plot_body_orbit(Moon, J2000)
-    frame.plot(lro, label='LRO')
+
+    plotter = StaticOrbitPlotter(plane=Planes.BODY_FIXED)
+    plotter.plot(lro, label='LRO')
     plt.show()
+
+if __name__ == "__main__":
+    plot_lro_orbit()
